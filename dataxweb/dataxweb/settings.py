799a0619dd15c00a010b35d872eb0cabc31dcd84
flackmaster <opencoding@hotmail.com>
2019-12-08 00:48:23 +0800

    增加admin字段配置

--- dataxweb/dataxweb/settings.py
+++ dataxweb/dataxweb/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'simpleui',
+    # 'django.contrib.admin.apps.SimpleAdminConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
